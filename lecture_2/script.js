36eb957953f65fb6f6944b01e1108417c5809171
Ashish <palashish73797@gmail.com>
2025-07-08 17:51:55 +0530

    initial commit

---
+++ lecture_2/script.js
@@ -0,0 +1,16 @@
+// Arithmatic Operators 
+let a = 5 ;
+let b = 2;
+// addition 
+let c = (a+b) ;
+console.log(c);
+
+// modules  -- get the remainder 
+console.log(a %b);
+
+// exponentiation -- get the power
+console.log('a ** b ', a**b);
+
+// increment (++)
+console.log(' a' ,a++); 
+console.log(' a+' ,a++); 
